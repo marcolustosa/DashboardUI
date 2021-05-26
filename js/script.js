1dc0e22566cd88b6a60677536bdb3459dfdf646f
Marco Lustosa <marco.lustosa@cartorio.local>
2021-05-26 15:23:01 -0300

    comit base

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$('.ui.dropdown')
+  .dropdown();
+
+$('.sidebar-menu-toggler').on('click', function() {
+    var target = $(this).data('target');
+    $(target)
+      .sidebar({
+        dinPage: true,
+        transition: 'overlay',
+        mobileTransition: 'overlay'
+      })
+      .sidebar('toggle');
+    });
